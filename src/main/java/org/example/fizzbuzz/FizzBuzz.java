b6f9cc5ba92d22176dd44a4d888ed2b9c7dd122c
Marsil <alashkar.marsil@gmail.com>
2019-11-25 14:32:45 +0100

    Erst

--- src/main/java/org/example/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/example/fizzbuzz/FizzBuzz.java
@@ -2,12 +2,24 @@ package org.example.fizzbuzz;
 
 public class FizzBuzz {
 
-    public static void main(String...args) {
-    	// TODO implement me
+    public static void main(String... args) {
+        for (int i=1; i<=100; i++) {
+            String fizzbuzznumber = fizzbuzz(i);
+            System.out.println(fizzbuzznumber);
+        }
     }
 
     public static String fizzbuzz(int number) {
-    	// TODO implement me
-	    return Integer.toString(15);
+        if (((number % 3) == 0) && ((number % 5) == 0)) {
+            return "FizzBuzz";
+        }
+        else if ((number % 3) == 0) {
+            return "Fizz";
+        } else if ((number % 5) == 0) {
+            return "Buzz";
+       
+        } else {
+            return Integer.toString(number);
+        }
     }
 }
--- src/test/java/org/example/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/example/fizzbuzz/FizzBuzzTest.java
@@ -12,8 +12,8 @@ public class FizzBuzzTest {
 
         List<String> expectedResults = Arrays.asList("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz");
         
-        for (int i = 0; i < expectedResults.size(); i++ ) {
-            String expected = expectedResults.get(i);
+        for (int i = 1; i < expectedResults.size(); i++ ) {
+            String expected = expectedResults.get(i-1);
             String actual =  FizzBuzz.fizzbuzz(i);
 
             Assert.assertEquals(expected, actual);
